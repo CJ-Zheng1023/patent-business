3cdaf2141ca3d1c112fd8e9dd8da8b2c3911aff1
AfterWin <CJ_Zheng1023@hotmail.com>
2016-06-30 15:57:27 +0800

    v0.5.0     header 完善，4屏完成

--- js/home.js
+++ js/home.js
@@ -1,10 +1,26 @@
 $(function(){
+
+    /*
+          绑定小屏时菜单下拉按钮点击事件
+     */
     $(".collapse-home").on("click",function(){
         var me=$(this);
         if(me.next().hasClass("active")){
             me.next().removeClass("active");
         }else{
             me.next().addClass("active");
+        }
+    })
+
+    /*
+         绑定二维码区域点击事件
+     */
+    $(".qrcode").on("click",function(){
+        var me=$(this);
+        if(me.hasClass("active")){
+            me.removeClass("active");
+        }else{
+            me.addClass("active");
         }
     })
 })
